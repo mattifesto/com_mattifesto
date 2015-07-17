cd388ac4e5048b520c4f5a06c3de3c23f480a261
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-07-17 15:36:41 -0700

    Add MDFloatingHeaderView::specToModel()

--- classes/MDFloatingHeaderView/MDFloatingHeaderView.php
+++ classes/MDFloatingHeaderView/MDFloatingHeaderView.php
@@ -2,4 +2,14 @@
 
 final class MDFloatingHeaderView {
 
+    /**
+     * @return {stdClass}
+     */
+    public static function specToModel(stdClass $spec) {
+        $model                          = MDModels::modelWithClassName(__CLASS__);
+        $model->color                   = isset($spec->color) ? trim($spec->color) : '';
+        $model->selectedMenuItemName    = isset($spec->selectedMenuItemName) ? trim($spec->selectedMenuItemName) : '';
+
+        return $model;
+    }
 }
