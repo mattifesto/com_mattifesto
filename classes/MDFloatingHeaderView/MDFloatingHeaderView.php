6c287eec035446a5ebbe844ec1d79c3a8268bde9
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-07-17 15:47:11 -0700

    Fix typo

--- classes/MDFloatingHeaderView/MDFloatingHeaderView.php
+++ classes/MDFloatingHeaderView/MDFloatingHeaderView.php
@@ -20,7 +20,7 @@ final class MDFloatingHeaderView {
      * @return {stdClass}
      */
     public static function specToModel(stdClass $spec) {
-        $model                          = MDModels::modelWithClassName(__CLASS__);
+        $model                          = CBModels::modelWithClassName(__CLASS__);
         $model->color                   = isset($spec->color) ? trim($spec->color) : '';
         $model->selectedMenuItemName    = isset($spec->selectedMenuItemName) ? trim($spec->selectedMenuItemName) : '';
 
