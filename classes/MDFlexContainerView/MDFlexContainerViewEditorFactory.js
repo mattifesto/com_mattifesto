c6bc506ad97beda8010275ce1a18cfb588bfcc1f
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2015-07-19 17:57:10 -0700

    MDFlexContainerView editor: Add options headers

--- classes/MDFlexContainerView/MDFlexContainerViewEditorFactory.js
+++ classes/MDFlexContainerView/MDFlexContainerViewEditorFactory.js
@@ -9,16 +9,20 @@ var MDFlexContainerViewEditorFactory = {
      * @return  {Element}
      */
     createEditor : function(args) {
-        var element         = document.createElement("div");
-        element.className   = "MDFlexContainerViewEditor";
-        var container       = document.createElement("div");
-        container.className = "container";
-        var preview         = CBImageEditorFactory.createImagePreviewElement();
-        preview.img.src     = args.spec.imageURL || "";
-        var options         = document.createElement("div");
-        options.className   = "options";
-        var subviews        = document.createElement("div");
-        subviews.className  = "subviews";
+        var element             = document.createElement("div");
+        element.className       = "MDFlexContainerViewEditor";
+        var container           = document.createElement("div");
+        container.className     = "container";
+        var preview             = CBImageEditorFactory.createImagePreviewElement();
+        preview.img.src         = args.spec.imageURL || "";
+        var options             = document.createElement("div");
+        options.className       = "options";
+        var background          = document.createElement("h2");
+        background.textContent  = "Background";
+        var flexbox             = document.createElement("h2");
+        flexbox.textContent     = "Flexbox";
+        var subviews            = document.createElement("div");
+        subviews.className      = "subviews";
 
         container.appendChild(preview.element);
 
@@ -34,6 +38,8 @@ var MDFlexContainerViewEditorFactory = {
             spec                : args.spec
         }));
 
+        options.appendChild(background);
+        options.appendChild(flexbox);
         container.appendChild(options);
 
         if (args.spec.subviews === undefined) {
