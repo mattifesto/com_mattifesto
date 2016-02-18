bcb702dcefc1e7b54d7cfeea08d9b6930e689abf
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-02-17 16:58:59 -0800

    Add missing font

--- classes/MDPageSettingsForResponsivePages/MDPageSettingsForResponsivePages.php
+++ classes/MDPageSettingsForResponsivePages/MDPageSettingsForResponsivePages.php
@@ -19,6 +19,7 @@ final class MDPageSettingsForResponsivePages {
         <link rel="stylesheet" href="<?= CBSystemURL ?>/css/equalize.css">
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet'>
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400' rel='stylesheet'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic' rel='stylesheet'>
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:700' rel='stylesheet'>
         <style>
             html { color: hsl(0, 0%, 20%); font-family: "Open Sans", "Helvetica Neue", "Helvetica", "Arial" }
