4aba050cf8bdad8aefc7cd9c5b1bbbdda51260dc
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2016-12-04 17:19:00 -0800

    Update MDStandardPageLayout
    - Added local styles.
    - Remove flex fill before footer so the main element will fill.

--- classes/MDStandardPageLayout/MDStandardPageLayout.php
+++ classes/MDStandardPageLayout/MDStandardPageLayout.php
@@ -9,23 +9,48 @@ final class MDStandardPageLayout {
      * @return null
      */
     public static function render(stdClass $layoutModel, callable $renderContentCallback) {
+        $stylesID = CBModel::value($layoutModel, 'stylesID');
+        $stylesCSS = CBModel::value($layoutModel, 'stylesCSS');
+
+        $classes[] = 'MDStandardPageLayout';
+        if (!empty($stylesID)) {
+            $classes[] = CBTheme::IDToCSSClass($stylesID);
+        }
+        $classes = implode(' ', $classes);
+
+        if (empty($stylesCSS)) {
+            $styleElement = '';
+        } else {
+            $styleElement = "<style>{$stylesCSS}</style>";
+        }
 
         CBThemedMenuView::renderModelAsHTML((object)[
             'menuID' => CBStandardModels::CBMenuIDForMainMenu,
             'themeID' => CBStandardModels::CBThemeIDForCBMenuViewForMainMenu,
         ]);
 
-        echo '<main class="MDStandardPageLayout">';
+        ?>
 
-        if (empty($layoutModel->hidePageTitleAndDescriptionView)) {
-            CBPageTitleAndDescriptionView::renderModelAsHTML((object)[]);
-        }
+        <main class="<?= $classes ?>" style="flex: 1 1 auto;">
+            <?= $styleElement ?>
 
-        $renderContentCallback();
+            <?php
 
-        echo '</main>';
+            if (empty($layoutModel->hidePageTitleAndDescriptionView)) {
+                CBPageTitleAndDescriptionView::renderModelAsHTML((object)[]);
+            }
 
-        MDStandardPageFooterView::renderModelAsHTML();
+            $renderContentCallback();
+
+            ?>
+
+        </main>
+
+        <?php
+
+        MDStandardPageFooterView::renderModelAsHTML((object)[
+            'hideFlexboxFill' => true,
+        ]);
     }
 
     /**
@@ -34,9 +59,16 @@ final class MDStandardPageLayout {
      * @return stdClass
      */
     public static function specToModel(stdClass $spec) {
-        return (object)[
+        $model = (object)[
             'className' => __CLASS__,
-            'hidePageTitleAndDescriptionView' => CBModel::value($spec, 'hidePageTitleAndDescriptionView', null, 'boolval'),
+            'hidePageTitleAndDescriptionView' => CBModel::value($spec, 'hidePageTitleAndDescriptionView', false, 'boolval'),
         ];
+
+        if (!empty($stylesTemplate = CBModel::value($spec, 'stylesTemplate', '', 'trim'))) {
+            $model->stylesID = CBHex160::random();
+            $model->stylesCSS = CBTheme::stylesTemplateToStylesCSS($stylesTemplate, $model->stylesID);
+        }
+
+        return $model;
     }
 }
--- classes/MDStandardPageLayoutEditor/MDStandardPageLayoutEditor.js
+++ classes/MDStandardPageLayoutEditor/MDStandardPageLayoutEditor.js
@@ -1,5 +1,7 @@
-"use strict";
-/* globals CBUI, CBUIBooleanEditor */
+"use strict"; /* jshint strict: global */
+/* globals
+    CBUI,
+    CBUIBooleanEditor */
 
 var MDStandardPageLayoutEditor = {
 
@@ -12,6 +14,21 @@ var MDStandardPageLayoutEditor = {
         item.appendChild(CBUIBooleanEditor.create({
             labelText : "Hide Page Title and Description View",
             propertyName : "hidePageTitleAndDescriptionView",
+            spec : args.spec,
+            specChangedCallback : args.specChangedCallback,
+        }).element);
+        section.appendChild(item);
+        element.appendChild(section);
+
+        /* local styles */
+
+        element.appendChild(CBUI.createHalfSpace());
+
+        section = CBUI.createSection();
+        item = CBUI.createSectionItem();
+        item.appendChild(CBUIStringEditor.createEditor({
+            labelText : "Styles Template",
+            propertyName : "stylesTemplate",
             spec : args.spec,
             specChangedCallback : args.specChangedCallback,
         }).element);
