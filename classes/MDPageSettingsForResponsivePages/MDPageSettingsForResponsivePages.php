554601e1fcedd424a4a9cc20816e406dda1469cc
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2017-12-30 16:02:40 -0800

    Make MDPageSettingsForResponsivePages use CBEqualize

--- classes/MDPageSettingsForResponsivePages/MDPageSettingsForResponsivePages.php
+++ classes/MDPageSettingsForResponsivePages/MDPageSettingsForResponsivePages.php
@@ -21,9 +21,6 @@ final class MDPageSettingsForResponsivePages {
      */
     static function renderHeadContent() { ?>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="<?= CBSystemURL ?>/javascript/html5shiv.js"></script>
-        <script src="<?= CBSystemURL ?>/javascript/ColbyEqualize.js"></script>
-        <link rel="stylesheet" href="<?= CBSystemURL ?>/css/equalize.css">
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400' rel='stylesheet'>
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic' rel='stylesheet'>
         <link href='https://fonts.googleapis.com/css?family=Open+Sans:600' rel='stylesheet'>
@@ -52,5 +49,12 @@ final class MDPageSettingsForResponsivePages {
             })(window,document,'script','dataLayer','<?= $googleTagManagerID ?>');</script>
             <!-- End Google Tag Manager -->
         <?php }
+    }
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_requiredHeadClassNames() {
+        return ['CBEqualize'];
     }
 }
