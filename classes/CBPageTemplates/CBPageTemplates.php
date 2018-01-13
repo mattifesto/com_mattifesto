c9f83ca04a9ad6a2924f80001a7fd47c45b5fd9e
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-01-12 18:21:26 -0800

    Add CBPageTemplates

---
+++ classes/CBPageTemplates/CBPageTemplates.php
@@ -0,0 +1,11 @@
+<?php
+
+final class CBPageTemplates {
+
+    /**
+     * @return [string]
+     */
+    static function templateClassNames() {
+        return ['MDBlogPostPageTemplate', 'CBStandardPageTemplate', 'CBEmptyPageTemplate'];
+    }
+}
