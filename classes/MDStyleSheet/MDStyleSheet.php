58536a234597d50b62f814fc4b2dc814a7535de9
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-03-08 09:58:06 -0800

    Add MDStyleSheet

---
+++ classes/MDStyleSheet/MDStyleSheet.php
@@ -0,0 +1,11 @@
+<?php
+
+final class MDStyleSheet {
+
+    /**
+     * @return [string]
+     */
+    static function CBHTMLOutput_CSSURLs() {
+        return [Colby::flexpath(__CLASS__, 'css', cbsiteurl())];
+    }
+}
