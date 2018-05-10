de69b48f0bc25d4ec6bd0183e16551dec17fefa7
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2018-05-10 10:58:44 -0700

    Add MDMainMenu

---
+++ classes/MDMainMenu/MDMainMenu.php
@@ -0,0 +1,38 @@
+<?php
+
+final class MDMainMenu {
+
+    /**
+     * @return void
+     */
+    static function CBInstall_configure(): void {
+        $originalSpec = CBModels::fetchSpecByID(MDMainMenu::ID());
+
+        if (empty($originalSpec)) {
+            $spec = (object)[
+                'ID' => MDMainMenu::ID(),
+                'title' => 'Mattifesto',
+                'titleURI' => '/',
+            ];
+        } else {
+            $spec = CBModel::clone($originalSpec);
+        }
+
+        $spec->className = 'CBMenu';
+
+        /* save if modified */
+
+        if ($spec != $originalSpec) {
+            CBDB::transaction(function () use ($spec) {
+                CBModels::save($spec);
+            });
+        }
+    }
+
+    /**
+     * @return ID
+     */
+    static function ID(): string {
+        return '56dbb447cf11414e5c3df442edc7ca6c3967a9ca';
+    }
+}
