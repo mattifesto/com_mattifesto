5d18acee67f6e9f46cd66abbd539b35164436dc4
Matt Calkins (MacBook Air) <matt@mattifesto.com>
2020-05-28 13:46:00 -0700

    Deprecate MDMostRecentBlogPostView

--- classes/MDMostRecentBlogPostView/MDMostRecentBlogPostView.php
+++ classes/MDMostRecentBlogPostView/MDMostRecentBlogPostView.php
@@ -1,70 +1,63 @@
 <?php
 
+/**
+ * @deprecated 2020_05_28
+ *
+ *      Can be removed after updated on each website.
+ */
 final class MDMostRecentBlogPostView {
 
+    /* -- CBInstall interfaces -- -- -- -- -- */
+
+
+
     /**
-     * @return object
+     * @return void
      */
-    private static function fetchSummary() {
-        $SQL = <<<EOT
-
-            SELECT `keyValueData`
-            FROM `ColbyPages`
-            WHERE `classNameForKind` = 'MDBlogPostPageKind'
-            AND `published` IS NOT NULL
-            ORDER BY `published` DESC
-            LIMIT 1
-
-EOT;
-
-        return CBDB::SQLToValue($SQL, ['valueIsJSON' => true]);
+    static function CBInstall_install(): void {
+        CBViewCatalog::installView(
+            __CLASS__,
+            (object)[
+                'isUnsupported' => true,
+            ]
+        );
     }
+    /* CBInstall_install() */
+
+
+
+    /**
+     * @return [string]
+     */
+    static function CBInstall_requiredClassNames(): array {
+        return [
+            'CBViewCatalog',
+        ];
+    }
+    /* CBInstall_requiredClassNames() */
+
+
+
+    /* -- CBView interfaces -- -- -- -- -- */
+
+
 
     /**
      * @param object $model
      *
      * @return null
      */
-    static function CBView_render(stdClass $model) {
-        if ($summary = MDMostRecentBlogPostView::fetchSummary()) {
-            $publishedAsHTML = ColbyConvert::timestampToHTML($summary->publicationTimeStamp, 'Unpublished');
-
-            ?>
-
-            <a class="MDMostRecentBlogPostView" href="/<?= $summary->URI ?>/">
-                <?php
-
-                $imageURL = CBImage::valueToFlexpath($summary, 'image', 'rw1280', cbsiteurl());
-
-                if ($imageURL !== false) {
-                    $image = $summary->image;
-
-                    CBArtworkElement::render([
-                        'height' => $image->height,
-                        'maxWidth' => 640,
-                        'URL' => $imageURL,
-                        'width' => $image->width,
-                    ]);
-                }
-
-                ?>
-
-                <h2><?= cbhtml($summary->title); ?></h2>
-                <div class="description"><?= cbhtml($summary->description) ?></div>
-                <div><span class="link">read more &gt;</span></div>
-            </a>
-
-            <?php
-        }
+    static function CBView_render(
+        stdClass $model
+    ): void {
+        echo '<!-- MDMostRecentBlogPostView -->';
     }
+    /* CBView_render() */
 
 
-    /**
-     * @return [string]
-     */
-    static function CBHTMLOutput_CSSURLs() {
-        return [Colby::flexpath(__CLASS__, 'css', cbsiteurl())];
-    }
+
+    /* -- CBModel interfaces -- -- -- -- -- */
+
 
 
     /**
@@ -72,9 +65,27 @@ EOT;
      *
      * @return object
      */
-    static function CBModel_build(stdClass $spec): stdClass {
-        return (object)[
-        ];
+    static function CBModel_build(
+        stdClass $spec
+    ): stdClass {
+        return (object)[];
     }
     /* CBModel_build() */
+
+
+
+    /**
+     * @param object
+     */
+    static function CBModel_upgrade(
+        stdClass $spec
+    ): stdClass {
+        return (object)[
+            'className' => 'CBPageListView2',
+            'classNameForKind' => 'MDBlogPostPageKind',
+            'CSSClassNames' => 'recent',
+        ];
+    }
+    /* CBModel_upgrade() */
+
 }
